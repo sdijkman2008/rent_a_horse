c91d993c1ce62645310ed7c9e5c5895ffd164b77
Sander Dijkman <sdijkman2008@gmail.com>
2019-11-14 15:36:00 +0100

    also added timeslot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_143237) do
+ActiveRecord::Schema.define(version: 2019_11_14_143458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_143237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.string "timeslot"
     t.index ["horse_id"], name: "index_reservations_on_horse_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
